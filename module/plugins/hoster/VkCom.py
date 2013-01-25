6bcab23710f38dfd06b275897f09bf89db489058
Albert De La Fuente <vonpupp@gmail.com>
2013-01-24 22:36:03 -0200

    changed headers

--- module/plugins/hoster/VkCom.py
+++ module/plugins/hoster/VkCom.py
@@ -4,14 +4,15 @@ import re
 from module.plugins.Hoster import Hoster
 from module.unescape import unescape
 
-class MyvideoDe(Hoster):
-    __name__ = "MyvideoDe"
+class VkCom(Hoster):
+    __name__ = "VkCom"
     __type__ = "hoster"
-    __pattern__ = r"http://(www\.)?myvideo.de/watch/"
-    __version__ = "0.9"
-    __description__ = """Myvideo.de Video Download Hoster"""
-    __author_name__ = ("spoob")
-    __author_mail__ = ("spoob@pyload.org")
+    __pattern__ = r"http://(www\.)?vk.com/video_ext.php/\?oid="
+    #http://vk.com/video_ext.php?oid=166335015&id=162608895&hash=b55affa83774504b&hd=1
+    __version__ = "0.1"
+    __description__ = """Vk.com Video Download Hoster"""
+    __author_name__ = ("vonpupp")
+    __author_mail__ = ("vonpupp@gmail.com")
         
     def setup(self):
         self.html = None
@@ -21,9 +22,11 @@ class MyvideoDe(Hoster):
         self.download_html()
         pyfile.name = self.get_file_name()
         self.download(self.get_file_url())
+        import ipdb; ipdb.set_trace()
 
     def download_html(self):
         self.html = self.load(self.pyfile.url)
+        import ipdb; ipdb.set_trace()
 
     def get_file_url(self):
         videoId = re.search(r"addVariable\('_videoid','(.*)'\);p.addParam\('quality'", self.html).group(1)
